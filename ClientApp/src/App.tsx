13d70847ae08e28455be49fbb2354d6ee1c526d8
Thomas Leclaire <thomas@leclaire.de>
2020-03-18 12:33:05 +0100

    Auf TypeScript umgestellt

---
+++ ClientApp/src/App.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import {Component} from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import { Counter } from "./components/Counter";
+
+import "./custom.css";
+
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  public render ():React.ReactElement {
+    return (
+      <Layout>
+        <Route exact path="/" component={Home} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/fetch-data" component={FetchData} />
+      </Layout>
+    );
+  }
+}
--- ClientApp/src/components/Counter.js
+++ ClientApp/src/components/Counter.js
@@ -1,31 +1,44 @@
-import React, { Component } from 'react';
-
-export class Counter extends Component {
-  static displayName = Counter.name;
-
-  constructor(props) {
-    super(props);
-    this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
-  }
-
-  incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Counter</h1>
-
-        <p>This is a simple example of a React component.</p>
-
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
-
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-      </div>
-    );
-  }
-}
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var react_1 = require("react");
+var Counter = /** @class */ (function (_super) {
+    __extends(Counter, _super);
+    function Counter(props) {
+        var _this = _super.call(this, props) || this;
+        _this.state = { currentCount: 0 };
+        _this.incrementCounter = _this.incrementCounter.bind(_this);
+        return _this;
+    }
+    Counter.prototype.incrementCounter = function () {
+        this.setState({
+            currentCount: this.state.currentCount + 1
+        });
+    };
+    Counter.prototype.render = function () {
+        return (React.createElement("div", null,
+            React.createElement("h1", null, "Counter"),
+            React.createElement("p", null, "This is a simple example of a React component."),
+            React.createElement("p", { "aria-live": "polite" },
+                "Current count: ",
+                React.createElement("strong", null, this.state.currentCount)),
+            React.createElement("button", { className: "btn btn-primary", onClick: this.incrementCounter }, "Increment")));
+    };
+    Counter.displayName = Counter.name;
+    return Counter;
+}(react_1.Component));
+exports.Counter = Counter;
+//# sourceMappingURL=Counter.js.map
